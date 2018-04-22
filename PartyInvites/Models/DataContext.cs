e3f19d06eda6ffd6028031e81ba21015456f0778
Barry Allison <bazallison@hotmail.com>
2018-04-23 05:36:06 +1200

    Add controller, Data Context, Modell, Views (with layout) and some configuration.

---
+++ PartyInvites/Models/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PertyInvites.Models
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+        
+        public DbSet<GuestResponse> Responses { get; set; }
+    }
+}
